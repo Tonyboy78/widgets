6a8ff55c6e44478dd161bd8c48f716b4f74c7a5b
Anton <tonyboy@live.se>
2021-02-20 00:49:18 +0100

    Added a new file Translate

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,76 +1,72 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('programming');
-    const [results, setResults] = useState([]);
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        const search = async () => {
-            const{data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term
-                }
-            });
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-            setResults(data.query.search);
-        };
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
-        if (term && !results.length) {
-            search();
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 1000);
-    
-            return () => {
-                clearTimeout(timeoutId);
-            };
-        }
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm,
+        },
+      });
 
-    }, [term]);
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
 
-    const renderedResults = results.map((result) => {
-        return (
-            <div key={result.pageid} className="item">
-                <div className="right floated content">
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
-                        Go
-                    </a>
-                </div>
-                <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
-                </div>
-            </div>
-        );
-    });
-
+  const renderedResults = results.map((result) => {
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input 
-                        value={term}
-                        onChange={e=> setTerm(e.target.value)}
-                        className="input" 
-                    />
-                </div>
-            </div>
-            <div className="ui celled list">
-                {renderedResults}
-            </div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
     );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
 };
 
 export default Search;
